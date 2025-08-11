fe0b4dc96674c85f69ca1a61552ff005ef5efe30
Nathaniel Johnston <48165052+nathanielwjohnston@users.noreply.github.com>
2025-08-11 20:47:52 +0100

    Link index router to template

--- controllers/indexController.js
+++ controllers/indexController.js
@@ -1,5 +1,18 @@
+const messages = [
+  {
+    text: "Alright mate",
+    user: "Steve",
+    added: new Date(),
+  },
+  {
+    text: "How's it going sir",
+    user: "John",
+    added: new Date(),
+  },
+];
+
 function getIndex(req, res) {
-  res.send("Page got");
+  res.render("index", { messages });
 }
 
 module.exports = { getIndex };
