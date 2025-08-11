18440f58415f61a2af07274f1a2857e2e7969b0d
Nathaniel Johnston <48165052+nathanielwjohnston@users.noreply.github.com>
2025-08-11 20:07:38 +0100

    Set up basic app

---
+++ app.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const app = express();
+const path = require("node:path");
+
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+
+const assetsPath = path.join(__dirname, "public");
+app.use(express.static(assetsPath));
+
+app.get("/", (req, res) => {
+  res.send("Page got");
+});
+
+const PORT = 8080;
+app.listen(PORT, () => {
+  console.log(`Message board - listening on port ${PORT}`);
+});
